cc075898ce0a6c247b614d5191a6fdb92502213b
mhkeller <michael.keller@gmail.com>
2014-03-22 18:58:46 -0400

    license, allow for archiving

--- TODO.md
+++ TODO.md
@@ -1,2 +1,1 @@
-* Optional push to bitbucket or alternate github
 * Configure web server either through `python -m SimpleHTTPServer` or with git-static
--- src/valdrada-listener.js
+++ src/valdrada-listener.js
@@ -8,21 +8,37 @@ function verifyAccount(incoming_repo){
 	if (incoming_repo == config.github_account) return true;
 	return false;
 }
+function determineArchiveRemoteUrl(domain){
+	domain = domain.toLowerCase().trim();
+	var tld = {
+		"bitbucket": ".org",
+		"github": ".com"
+	}
+	return domain + tld[domain]
+
+}
 function createDirGitInit(info){
 	var repo_name = info.repository.name;
 	fs.mkdirSync('./' + repo_name);
-        sh.run('cd ' + repo_name + ' && git init && git remote add origin ' + info.repository.url + '.git');
+
+	var create_statement = 'cd ' + repo_name + ' && git init && git remote add origin ' + info.repository.url + '.git';
+	if (config.archive.enabled) create_statement += ' && git remote add archived https://' + determineArchiveRemoteUrl(config.archive.type) + '/' + config.archive.account_name + '/' + repo_name + '.git';
+  
+  sh.run(create_statement);
 }
 function pullLatest(info){
 	var repo_name = info.repository.name;
 	if (!fs.existsSync('./' + repo_name)){
-		createDirGitInit(info)
+		createDirGitInit(info);
 	}
-	sh.run('cd ' + repo_name + ' &&  git fetch origin && git checkout origin/master');
+
+	var pull_statement = 'cd ' + repo_name + ' &&  git fetch origin && git checkout origin/master';
+	if (config.archive.enabled) pull_statement += ' && git push origin archived';
+	sh.run(pull_statement);
 }
-function checkForDeployMsg(info){
+function checkForDeployMsg(commits){
 	// The last commit in the array is the most recent
-	var commit_msg = info.commits[info.commits.length - 1].message,
+	var commit_msg = commits[commits.length - 1].message,
 	    deploy_regx = new RegExp(config.deploy_trigger);
 	if (deploy_regx.exec(commit_msg)) return true;
 	return false;
@@ -31,18 +47,20 @@ function deployToS3(info){
 	var repo_name = info.repository.name,
 	    path      = (config.path) ? config.path + '/' : '';
 
-
 	var deploy_result = sh.exec('aws s3 sync '+repo_name+' s3://'+config.bucket_name+'/'+path+repo_name+'/ --exclude ".git/*" --exclude ".*"');
 	console.log(deploy_result.stdout);
 }
 hookshot('refs/heads/master', function(info){
-	var account_verified = verifyAccount(info.repository.owner.name);
-	if (account_verified){
+	var is_account_verified = verifyAccount(info.repository.owner.name),
+	    deploy_msg_found    = checkForDeployMsg(info.commits);
+
+	if (is_account_verified){
 		pullLatest(info);
-		var deploy_msg = checkForDeployMsg(info);
-		if (deploy_msg){
+
+		if (deploy_msg_found){
 			deployToS3(info);
 		}
+
 	}
 }).listen(config.port);
 console.log('Listening on port... ' + config.port);	
