b2ab50a1ac6d071087c71c1f6ebcdbda29f6a220
mhkeller <michael.keller@gmail.com>
2014-03-29 18:52:27 -0400

    remove lines, add parens

--- src/sh-commands.js
+++ src/sh-commands.js
@@ -15,16 +15,17 @@ var helpers = {
 }
 var sh_commands = {
 	createGitRepoAndRemotes: function(repo_name, url, archive_info){
-		return 'cd repositories/' + repo_name + ' && git init && git remote add origin ' + url + '.git' + (archive_info.enabled) ? this.setArchiveRemote(repo_name, archive_info) : '';
+		return 'cd repositories/' + repo_name + ' && git init && git remote add origin ' + url + '.git' + ((archive_info.enabled) ? this.setArchiveRemote(repo_name, archive_info) : '');
 	},
 	setArchiveRemote: function(repo_name, archive_info){
 		return ' && git remote add archived https://' + helpers.determineArchiveRemoteUrl(archive_info.type) + '/' + archive_info.account_name + '/' + repo_name + '.git';
 	},
 	fetchLatest: function(repo_name, archive_info){
-		return 'cd repositories/' + repo_name + ' && git fetch origin && git checkout origin/master' + (archive_info.enabled) ? this.pushToArchive() : '';
+		// return '';
+		return 'cd repositories/' + repo_name + ' && git fetch origin && git checkout origin/master' + ((archive_info.enabled) ? this.pushToArchive() : '');
 	},
 	pushToArchive: function(){
-		return ' && git push origin archived';
+		return ' && git push archived';
 	},
 	deploy: function(mode, repo_name, bucket_name, path, exclusions){
 		return 'aws s3 ' + mode + ' '+repo_name+' s3://'+bucket_name+'/'+path+repo_name+'/ --acl public-read ' + ((mode == 'cp') ? '--recursive' : '') + ' ' + helpers.excludeFiles(exclusions)
