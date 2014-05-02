fa6178887301b9708c537b3478fa6cddff2a4ce1
mhkeller <michael.keller@gmail.com>
2014-05-02 19:20:22 -0400

    better branch tracking

--- src/sh-commands.js
+++ src/sh-commands.js
@@ -15,10 +15,10 @@ var helpers = {
 }
 var sh_commands = {
 	createGitRepoAndRemotes: function(url){
-		return 'cd repositories && git clone ' + url + '.git';
+		return 'cd repositories && git clone ' + url + '.git && for remote in $(git branch -r) ; do git branch --track $(echo $remote | cut -d '/' -f2) remotes/$remote; done';
 	},
 	fetchLatest: function(repo_name){
-		return 'cd repositories/' + repo_name + ' && git fetch';
+		return 'cd repositories/' + repo_name + ' && git fetch --all && git pull --all';
 	},
 	deploy: function(mode, repo_name, bucket_name, path, exclusions){
 		return 'aws s3 ' + mode + ' '+repo_name+' s3://'+bucket_name+'/'+path+repo_name+'/ --acl public-read ' + ((mode == 'cp') ? '--recursive' : '') + ' ' + helpers.excludeFiles(exclusions)
