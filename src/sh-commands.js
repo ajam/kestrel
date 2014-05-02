92531abc7a860a8f423f7d52cc6b2baa321b4c21
mhkeller <michael.keller@gmail.com>
2014-05-02 19:05:04 -0400

    only fetch

--- src/sh-commands.js
+++ src/sh-commands.js
@@ -18,7 +18,7 @@ var sh_commands = {
 		return 'cd repositories/' + repo_name + ' && git clone ' + url + '.git';
 	},
 	fetchLatest: function(repo_name){
-		return 'cd repositories/' + repo_name + ' && git fetch origin && git checkout origin/master';
+		return 'cd repositories/' + repo_name + ' && git fetch';
 	},
 	deploy: function(mode, repo_name, bucket_name, path, exclusions){
 		return 'aws s3 ' + mode + ' '+repo_name+' s3://'+bucket_name+'/'+path+repo_name+'/ --acl public-read ' + ((mode == 'cp') ? '--recursive' : '') + ' ' + helpers.excludeFiles(exclusions)
