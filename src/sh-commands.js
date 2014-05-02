4c2bf41e00a7a81a36b3aa2e03a40ac57453bbd0
mhkeller <michael.keller@gmail.com>
2014-05-02 19:42:26 -0400

    pwd test

--- src/sh-commands.js
+++ src/sh-commands.js
@@ -18,12 +18,12 @@ var sh_commands = {
 		console.log('url',url);
 		return 'cd repositories && git clone ' + url + '.git';
 	},
+	fetchLatest: function(repo_name){
+		return 'pwd && cd repositories/' + repo_name + ' && git fetch --all && git pull --all';
+	},
 	trackAllBranches: function(repo_name){
 		console.log('repo-name', repo_name)
-		return 'cd repositories/' + repo_name + ' && for remote in $(git branch -r) ; do git branch --track $(echo $remote | cut -d '/' -f2) remotes/$remote; done';
-	},
-	fetchLatest: function(repo_name){
-		return 'cd repositories/' + repo_name + ' && git fetch --all && git pull --all';
+		return 'pwd && for remote in $(git branch -r) ; do git branch --track $(echo $remote | cut -d '/' -f2) remotes/$remote; done';
 	},
 	deploy: function(mode, repo_name, bucket_name, path, exclusions){
 		return 'aws s3 ' + mode + ' '+repo_name+' s3://'+bucket_name+'/'+path+repo_name+'/ --acl public-read ' + ((mode == 'cp') ? '--recursive' : '') + ' ' + helpers.excludeFiles(exclusions)
