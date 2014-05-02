bb4611ffafb29dfc14bc0a8668cde046a79e3f25
mhkeller <michael.keller@gmail.com>
2014-05-02 17:23:15 -0400

    remove archive options

--- src/sh-commands.js
+++ src/sh-commands.js
@@ -14,17 +14,11 @@ var helpers = {
 	}
 }
 var sh_commands = {
-	createGitRepoAndRemotes: function(repo_name, url, archive_info){
-		return 'cd repositories/' + repo_name + ' && git init && git remote add origin ' + url + '.git' + ((archive_info.enabled) ? this.setArchiveRemote(repo_name, archive_info) : '');
+	createGitRepoAndRemotes: function(repo_name, url){
+		return 'cd repositories/' + repo_name + ' && git init && git remote add origin ' + url + '.git';
 	},
-	setArchiveRemote: function(repo_name, archive_info){
-		return ' && git remote add archived https://' + helpers.determineArchiveRemoteUrl(archive_info.type) + '/' + archive_info.account_name + '/' + repo_name + '.git';
-	},
-	fetchLatest: function(repo_name, archive_info){
-		return 'cd repositories/' + repo_name + ' && git fetch origin && git checkout origin/master' + ((archive_info.enabled) ? this.pushToArchive() : '');
-	},
-	pushToArchive: function(){
-		return ' && git push archived';
+	fetchLatest: function(repo_name){
+		return 'cd repositories/' + repo_name + ' && git fetch origin && git checkout origin/master';
 	},
 	deploy: function(mode, repo_name, bucket_name, path, exclusions){
 		return 'aws s3 ' + mode + ' '+repo_name+' s3://'+bucket_name+'/'+path+repo_name+'/ --acl public-read ' + ((mode == 'cp') ? '--recursive' : '') + ' ' + helpers.excludeFiles(exclusions)
