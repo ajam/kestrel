90fc5931a5c2ee05a6a0311657b2aac82317b500
mhkeller <michael.keller@gmail.com>
2014-05-02 20:14:24 -0400

    checkout all

--- src/sh-commands.js
+++ src/sh-commands.js
@@ -18,10 +18,10 @@ var sh_commands = {
 		return 'cd repositories/'+repo_name+' && git remote add origin ' + url + '.git';
 	},
 	fetchLatest: function(repo_name){
-		return 'cd repositories/' + repo_name + ' && git fetch --all && git pull --all';
+		return 'cd repositories/' + repo_name + ' && git fetch origin && git checkout origin/master';
 	},
 	trackAllBranches: function(repo_name){
-		return 'cd repositories/' + repo_name + ' && for remote in $(git branch -r) ; do git branch --track $(echo $remote | cut -d \'/\' -f2) remotes/$remote; done';
+		return 'cd repositories/' + repo_name + ' && for remote in $(git branch -r) ; do git checkout $(echo $remote | cut -d \'/\' -f2) remotes/$remote; done';
 	},
 	deploy: function(mode, repo_name, bucket_name, path, exclusions){
 		return 'aws s3 ' + mode + ' '+repo_name+' s3://'+bucket_name+'/'+path+repo_name+'/ --acl public-read ' + ((mode == 'cp') ? '--recursive' : '') + ' ' + helpers.excludeFiles(exclusions)
